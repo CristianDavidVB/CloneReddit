36d4f04a6bde412c7d1ec701764630093f10a6ef
cristian <cristiandavidv@hotmail.com>
2023-03-04 22:17:47 -0500

    controller added and working in Comments

--- service-comments/src/main/java/com/clone/service/comments/services/CommentServiceImp.java
+++ service-comments/src/main/java/com/clone/service/comments/services/CommentServiceImp.java
@@ -1,2 +1,32 @@
-package com.clone.service.comments.services;public class CommentServiceImp {
+package com.clone.service.comments.services;
+
+import com.clone.service.comments.dtos.CommentDTO;
+import com.clone.service.comments.models.Comment;
+import com.clone.service.comments.repositories.CommentRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Service
+public class CommentServiceImp implements CommentService{
+
+    @Autowired
+    private CommentRepository commentRepository;
+
+    @Autowired
+    private ModelMapper modelMapper;
+    @Override
+    public List<CommentDTO> findAll() {
+        List<Comment>comments = commentRepository.findAll();
+        return comments.stream()
+                .map(comment -> modelMapper.map(comment, CommentDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public CommentDTO findById(Long id) {
+        return null;
+    }
 }
