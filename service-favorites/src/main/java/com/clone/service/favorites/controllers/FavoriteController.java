69c9abbba948f3f4e5270366221711b11821e7cc
cristian <cristiandavidv@hotmail.com>
2023-03-04 21:33:20 -0500

    controller added and working in Favorites

--- service-favorites/src/main/java/com/clone/service/favorites/controllers/FavoriteController.java
+++ service-favorites/src/main/java/com/clone/service/favorites/controllers/FavoriteController.java
@@ -1,2 +1,23 @@
-package com.clone.service.favorites.controllers;public class FavoriteController {
+package com.clone.service.favorites.controllers;
+
+import com.clone.service.favorites.dtos.FavoriteDTO;
+import com.clone.service.favorites.services.FavoriteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/favorites")
+public class FavoriteController {
+    @Autowired
+    private FavoriteService favoriteService;
+
+    @GetMapping
+    public ResponseEntity<List<FavoriteDTO>> findAll(){
+        return  ResponseEntity.ok(favoriteService.findAll());
+    }
 }
--- service-favorites/src/main/java/com/clone/service/favorites/models/Favorite.java
+++ service-favorites/src/main/java/com/clone/service/favorites/models/Favorite.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 @Entity
-@Table(name = "favories")
+@Table(name = "favorites")
 @Data
 @AllArgsConstructor @NoArgsConstructor
 public class Favorite {
