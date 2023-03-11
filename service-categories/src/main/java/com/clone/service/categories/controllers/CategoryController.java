b2597b805a996e59f9638861173333eaf747ef20
cristian <cristiandavidv@hotmail.com>
2023-03-11 17:31:14 -0500

    CRUD COMPLETE Categories and SubCategories

--- service-categories/src/main/java/com/clone/service/categories/controllers/CategoryController.java
+++ service-categories/src/main/java/com/clone/service/categories/controllers/CategoryController.java
@@ -3,6 +3,8 @@ package com.clone.service.categories.controllers;
 import com.clone.service.categories.dtos.CategoryDTO;
 import com.clone.service.categories.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,10 +18,22 @@ public class CategoryController {
     @GetMapping
     public ResponseEntity<List<CategoryDTO>> findAll(){return ResponseEntity.ok(categoryService.findAll());
     }
+
+    //Parametro de ruta
+    @GetMapping("/{id}")
+    public  ResponseEntity<CategoryDTO> findById(@PathVariable("id") Long id){
+        return ResponseEntity.ok(categoryService.findById(id));
+    }
     //ResponseEntity para proveer una peticionn completa(Body, header, status)
     @PostMapping
     public ResponseEntity<CategoryDTO> create(@RequestBody CategoryDTO categoryDTO){
-        return ResponseEntity.ok(categoryService.create(categoryDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(categoryService.create(categoryDTO));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryDTO> update(@PathVariable("id") Long id,
+                                              @RequestBody CategoryDTO categoryDTO){
+        return ResponseEntity.ok(categoryService.update(id, categoryDTO));
     }
 
     @DeleteMapping("/{id}")
--- service-categories/src/main/java/com/clone/service/categories/services/SubCategoryServiceImp.java
+++ service-categories/src/main/java/com/clone/service/categories/services/SubCategoryServiceImp.java
@@ -3,6 +3,7 @@ package com.clone.service.categories.services;
 import com.clone.service.categories.dtos.SubCategoryDTO;
 import com.clone.service.categories.models.SubCategory;
 import com.clone.service.categories.repositories.SubCategoryRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -27,7 +28,9 @@ public class SubCategoryServiceImp implements SubCategoryService{
 
     @Override
     public SubCategoryDTO findById(Long id) {
-        return null;
+        SubCategory subCategory = subCategoryRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Subcategorie not found with by id" + id));
+        return modelMapper.map(subCategory, SubCategoryDTO.class);
     }
 
     @Override
@@ -36,6 +39,15 @@ public class SubCategoryServiceImp implements SubCategoryService{
         SubCategory saveSubCategory = subCategoryRepository.save(subCategory);
         return modelMapper.map(saveSubCategory, SubCategoryDTO.class);
     }
+
+   @Override
+   public SubCategoryDTO update(Long id, SubCategoryDTO subCategoryDTO) {
+        SubCategory subCategory = subCategoryRepository.getReferenceById(id);
+        subCategory.setName(subCategoryDTO.getName());
+        SubCategory updateSubCategory = subCategoryRepository.save(subCategory);
+        return modelMapper.map(updateSubCategory, SubCategoryDTO.class);
+   }
+
     @Override
     public void delete(Long id) {
         subCategoryRepository.deleteById(id);
