bc1946b3890fde9694fee206d801750a9ed9165a
cristian <cristiandavidv@hotmail.com>
2023-03-12 12:04:21 -0500

    CRUD completed for Like and refactorice

--- service-likes/src/main/java/com/clone/service/likes/services/LikeServiceImp.java
+++ service-likes/src/main/java/com/clone/service/likes/services/LikeServiceImp.java
@@ -3,6 +3,7 @@ package com.clone.service.likes.services;
 import com.clone.service.likes.dtos.LikeDTO;
 import com.clone.service.likes.models.Like;
 import com.clone.service.likes.repositories.LikeRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,19 +22,45 @@ public class LikeServiceImp implements LikeService{
     public List<LikeDTO> findAll() {
         List<Like> likes = likeRepository.findAll();
         return likes.stream()
-                .map(like -> modelMapper.map(like, LikeDTO.class))
+                .map(this::convertToDTO)
                 .collect(Collectors.toList());
     }
 
     @Override
     public LikeDTO findById(Long id) {
-        return null;
+        Like like = likeRepository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException("Like not found with id" + id));
+        return convertToDTO(like);
     }
 
     @Override
     public LikeDTO create(LikeDTO likeDTO) {
-        Like like = modelMapper.map(likeDTO, Like.class);
+        Like like = convertToEntity(likeDTO);
         Like saveLike = likeRepository.save(like);
-        return modelMapper.map(saveLike, LikeDTO.class);
+        return convertToDTO(saveLike);
+    }
+
+    @Override
+    public LikeDTO update(Long id, LikeDTO likeDTO){
+        Like like = likeRepository.getReferenceById(id);
+        like.setNumber_like(likeDTO.getNumber_like());
+        Like updateLike = likeRepository.save(like);
+        return convertToDTO(updateLike);
+    }
+
+    @Override
+    public void delete(Long id){likeRepository.deleteById(id);}
+
+    private LikeDTO convertToDTO(Like like){
+        LikeDTO likeDTO = new LikeDTO();
+        likeDTO.setId(like.getId());
+        likeDTO.setNumber_like(like.getNumber_like());
+        return likeDTO;
+    }
+    private Like convertToEntity(LikeDTO likeDTO){
+        Like like = new Like();
+        like.setId(like.getId());
+        like.setNumber_like(likeDTO.getNumber_like());
+        return like;
     }
 }
